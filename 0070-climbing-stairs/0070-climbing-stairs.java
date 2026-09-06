eaab30fa82c3fd8a6111242097f3409b637fc78d
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2026-09-06 17:16:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<1){
-            return 1;
-        }
-        ArrayList<Integer> m= new ArrayList<>(Collections.nCopies(n+1,0));
-        m.set(0,1);
-        m.set(1,1);
+        int [] dp= new int [n+1];
+         dp[0]=1;
+        dp[1]=1;
         for(int i=2;i<=n;i++){
-            m.set(i,(m.get(i-1)+m.get(i-2)));
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return m.get(n);
+        return dp[n];
     }
 }
